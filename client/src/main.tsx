80e20d0d745eba762734d7a39c49f644c8edc8ce
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-02-14 16:01:48 -0300

    Add date and slot selection for user

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
-
 import { BookSlotPage, HomePage } from './pages';
-
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/book-slot" element={<BookSlotPage />} />
+        <Route path="/" element={<HomePage />}>
+          <Route path="/book-slot" element={<BookSlotPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,19 +1,48 @@
-import { useNavigate } from 'react-router';
 import { Box, Button, Typography } from '@mui/material';
+import { Outlet, useNavigate } from 'react-router';
+import { LocalizationProvider } from '../providers';
 
 export const HomePage = () => {
   const navigate = useNavigate();
 
   return (
-    <Box sx={{ width: '100%', textAlign: 'center' }}>
-      <Typography variant="h1">Welcome!</Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => navigate('/book-slot')}
+    <LocalizationProvider>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100vh',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 4,
+          padding: 4,
+          background: 'linear-gradient(to right, #f8f9fa, #eef1f5)',
+        }}
       >
-        Book a Slot
-      </Button>
-    </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: 4,
+            padding: 6,
+            backgroundColor: 'white',
+            borderRadius: 3,
+            boxShadow:1
+          }}
+        >
+          <Typography variant="h2">Welcome to Appointment Booking</Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => navigate('/book-slot')}
+          >
+            Book a Slot
+          </Button>
+        </Box>
+        <Outlet />
+      </Box>
+     </LocalizationProvider>
   );
 };
