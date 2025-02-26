ad3321f480f1dd0657bb26a87d7b7e67ff769c1e
thanwin84 <thanwinhline84@gmail.com>
2025-02-26 23:32:10 +0600

    add appointment schema

---
+++ server/src/constants.js
@@ -0,0 +1,11 @@
+const role = ['customer', 'admin', 'provider'];
+const appointmentStatus = {
+  COMPLETED: 'completed',
+  SCHEDULED: 'scheduled',
+  CANCELLED: 'cancelled',
+};
+
+module.exports = {
+  role,
+  appointmentStatus,
+};
