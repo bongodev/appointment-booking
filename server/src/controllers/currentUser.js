9430fd5f3db82d85ec8439384194d051edda82e3
kmtusher97 <kmtusher97@gmail.com>
2025-04-11 21:56:01 +0600

    Fix server formatting errors

--- server/src/controllers/currentUser.js
+++ server/src/controllers/currentUser.js
@@ -2,37 +2,40 @@ import asyncHandler from 'express-async-handler';
 import { getCurrentUserById } from '../services/index.js';
 
 export const getcurrentUser = asyncHandler(async (req, res) => {
-  
-    const userId = req.user._id;
-    if (!userId) {
-      return res.status(400).json({
-        message: 'Not authenticiated',
-      });
-    }
-    const {currentUser, currentUserProfile} = await getCurrentUserById(userId);
-    if (!currentUser){
-      return res.status(404).json({
-        success: false,
-        message: 'User not found',
-      });  
-    }
-    
-    res.status(200).json({
-      id: currentUser._id,
-      name: currentUser.name,
-      email: currentUser.email,
-      mobile: currentUser.mobile,
-      role: currentUser.role? {
-        id: currentUser.role._id,
+  const userId = req.user._id;
+  if (!userId) {
+    return res.status(400).json({
+      message: 'Not authenticiated',
+    });
+  }
+  const { currentUser, currentUserProfile } = await getCurrentUserById(userId);
+  if (!currentUser) {
+    return res.status(404).json({
+      success: false,
+      message: 'User not found',
+    });
+  }
+
+  res.status(200).json({
+    id: currentUser._id,
+    name: currentUser.name,
+    email: currentUser.email,
+    mobile: currentUser.mobile,
+    role: currentUser.role
+      ? {
+          id: currentUser.role._id,
           name: currentUser.role.role,
           description: currentUser.role.description,
-          permissions: currentUser.role.permissions
-      } : null,
-      profile: currentUserProfile? {
-        id: currentUserProfile._id,
-        image: currentUserProfile.image,
-        address: currentUserProfile.address,
-        dateOfBirth: currentUserProfile.dateOfBirth
-      } : null
-    });
+          permissions: currentUser.role.permissions,
+        }
+      : null,
+    profile: currentUserProfile
+      ? {
+          id: currentUserProfile._id,
+          image: currentUserProfile.image,
+          address: currentUserProfile.address,
+          dateOfBirth: currentUserProfile.dateOfBirth,
+        }
+      : null,
   });
+});
--- server/src/services/currentUser.js
+++ server/src/services/currentUser.js
@@ -1,11 +1,14 @@
 import { User, UserProfile } from '../models/index.js';
 export const getCurrentUserById = async (userId) => {
-
-    const currentUser = await User.findById(userId).select('-password').populate({path: 'role',select: 'role description permissions'});
-    if (!currentUser) {
-      throw new Error('User not found');
-    }
-    const currentUserProfile = await UserProfile.findOne({user: userId,
-      deleted: false });
-    return {currentUser, currentUserProfile};
+  const currentUser = await User.findById(userId)
+    .select('-password')
+    .populate({ path: 'role', select: 'role description permissions' });
+  if (!currentUser) {
+    throw new Error('User not found');
+  }
+  const currentUserProfile = await UserProfile.findOne({
+    user: userId,
+    deleted: false,
+  });
+  return { currentUser, currentUserProfile };
 };
