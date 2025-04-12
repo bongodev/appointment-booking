6daa1b1d696c7c81e56bf220641cd914c1c9dcc4
thanwin84 <thanwinhline84@gmail.com>
2025-04-13 01:12:04 +0600

    Add notFoundError classs

--- server/src/utils/customErrors.js
+++ server/src/utils/customErrors.js
@@ -6,3 +6,12 @@ export class DuplicateResourceError extends Error {
     Error.captureStackTrace(this, this.constructor);
   }
 }
+
+export class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = 404;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
