04d5fa6c8cd788365f4994457ef44e1800f737f2
thanwin84 <thanwinhline84@gmail.com>
2025-05-15 03:19:59 +0600

    Add reusable top navbar for dashboard layout - Support centered content via children prop - Toggle sidebar on small screens - Hide logo on small screens for cleaner UI

---
+++ client/src/components/DashboardTopNavbar.tsx
@@ -0,0 +1,67 @@
+import { AppBar, Toolbar, MenuIcon, IconButton, Box } from '@/ui';
+
+import { ReactNode } from 'react';
+
+import { uiConfig } from '@/config';
+import { UserAvatarMenu } from './navigation';
+import { useSidebar } from '@/providers/SidebarProvider';
+import AppLogo from './AppLogo';
+
+export default function DashboardTopNavbar({
+  children,
+}: {
+  children?: ReactNode;
+}) {
+  const { toggleSidebar } = useSidebar();
+  return (
+    <AppBar
+      sx={{
+        height: uiConfig.appBarHeight,
+
+        backgroundColor: 'primary.main', // optional: makes background solid
+      }}
+    >
+      <Toolbar
+        sx={{
+          display: 'flex',
+          gap: 2,
+          justifyContent: 'space-between',
+          width: '100%',
+          px: 4,
+        }}
+        disableGutters
+      >
+        <IconButton
+          onClick={toggleSidebar}
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{
+            mr: 2,
+            display: {
+              xs: 'inline-flex',
+              sm: 'inline-flex',
+              md: 'none',
+            },
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Box
+          sx={{
+            display: {
+              xs: 'none',
+              sm: 'none',
+              md: 'block',
+            },
+          }}
+        >
+          <AppLogo />
+        </Box>
+
+        <Box sx={{ width: '100%' }}>{children}</Box>
+        <UserAvatarMenu />
+      </Toolbar>
+    </AppBar>
+  );
+}
