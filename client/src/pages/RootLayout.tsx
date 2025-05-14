36eebe60d83cdb3f5f2060cb87b20b2143b4fa4a
thanwin84 <thanwinhline84@gmail.com>
2025-05-15 03:47:26 +0600

    update root layout

--- client/src/pages/RootLayout.tsx
+++ client/src/pages/RootLayout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from 'react-router';
 import { MuiLocalizationProvider } from '@/providers';
 
 import { Box, Stack } from '@/ui';
+import { TopNavBar } from '@/components/navigation';
 
 export default function RootLayout() {
   return (
     <MuiLocalizationProvider>
       <Stack>
+        <TopNavBar />
         <Box sx={{ mt: 8 }}>
           <Outlet />
         </Box>
