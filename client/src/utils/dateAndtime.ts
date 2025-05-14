d46fe4908fefdbec704451bb41a9a25e3b289257
thanwin84 <thanwinhline84@gmail.com>
2025-05-15 02:40:05 +0600

    Create date and time utility function for slot page

--- client/src/utils/dateAndtime.ts
+++ client/src/utils/dateAndtime.ts
@@ -1,14 +1,17 @@
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 
-export const getWeekDates = () => {
-  const today = dayjs();
-  const startOfWeek = today.startOf('week');
-  return Array.from({ length: 7 }, (_, i) => startOfWeek.add(i, 'day'));
+export const getWeekDates = (weekOffset: number = 0): Dayjs[] => {
+  const startOfTargetWeek = dayjs().startOf('week').add(weekOffset, 'week');
+  return Array.from({ length: 7 }, (_, i) => startOfTargetWeek.add(i, 'day'));
 };
-
 export const getWeekLabel = (weekDates: dayjs.Dayjs[]) => {
   const isSameMonth = weekDates[0].month() === weekDates[6].month();
   return isSameMonth
     ? weekDates[0].format('MMMM YYYY')
     : `${weekDates[0].format('MMM D')} – ${weekDates[6].format('MMM D, YYYY')}`;
+};
+
+export const getMonthAndYear = (date?: Dayjs): string => {
+  const targetDate = date || dayjs();
+  return targetDate.format('MMMM YYYY');
 };
